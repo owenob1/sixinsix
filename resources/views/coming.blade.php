fc1fd451837e16258815856e0be6ef44b5f670c2
Unknown <owenob1@gmail.com>
2018-03-22 02:39:22 +1100

    Launch!

--- resources/views/coming.blade.php
+++ resources/views/coming.blade.php
@@ -37,6 +37,40 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Section -->
+		<section>
+			<div class="container">
+
+				<div class="row">
+					<div class="col-lg-8 col-lg-push-2">
+
+						<!-- Accordion -->
+						<div class="panel-group" role="tablist" aria-multiselectable="true">
+							<!-- Panel -->
+							<div class="panel panel-2">
+								<div class="panel-heading" role="tab">
+								    <h4 class="panel-title text-center">
+								    	<a data-toggle="collapse" href="#collapseOneAlt" aria-expanded="false"><h3>>How does it works?</h3></a>
+								    </h4>
+								</div>
+								<div id="collapseOneAlt" class="panel-collapse collapse" role="tabpanel">
+							  		<div class="panel-body">
+							    	Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+							  		</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+
+	</div>
+	<!-- Content / End -->
+
 	</section>
 	<!--END Header-->
 	<!-- Footer / End -->
