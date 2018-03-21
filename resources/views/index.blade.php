a01702ec4cd98187a452d3bc12964e5ad10df39e
Unknown <owenob1@gmail.com>
2018-03-22 03:20:31 +1100

    Navbar Bug Fixed!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
 	<div id="page-loader"><svg class="loader-1 loader-primary" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="circle" fill="none" stroke-width="3" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg></div>
 	<!-- Loader / End -->
 	<!-- Header -->
-	<header id="header">
+	<header id="header" class="absolute">
 		<!-- Navigation Bar -->
 		<div id="nav-bar">
 			<!-- Menu Toggle -->
