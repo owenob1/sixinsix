b90dc26f3510b44d81ef900c5376dcc8c3c9bfd6
Unknown <owenob1@gmail.com>
2018-07-02 20:16:07 +1000

    cb
    dfg

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Blog;
+class BlogController extends Controller
+{
+  public function index(){
+      $blog = Blog::whereRaw(true)->paginate('15');
+    return view('frontend.blog')->with(['blog' => $blog]);
+  }
+  public function item($slug) {
+//      $blog =Blog::where('title', $slug)->first();
+      $blog = Blog::findOrFail($slug);
+      return view('frontend.blogItem')->with(['blog' => $blog]);
+  }
+}
